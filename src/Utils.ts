606bc877d1835998f1dbc321da81e1cdc2be50e4
Reto Kunz <reto.kunz@ost.ch>
2024-09-05 22:51:43 +0200

    refactor authentication function, closes #47

--- src/Utils.ts
+++ src/Utils.ts
@@ -1,6 +1,7 @@
 import { RequestHandler } from "express";
 import { Card } from "./models/Card";
 import * as jose from "jose";
+import { accessToken } from "./models/AccessToken";
 const secret = new TextEncoder().encode("myDarkSecret");
 
 export const shuffel = (array: Card[]): void => {
@@ -43,8 +44,8 @@ export const authorize =
 
       try {
         //TODO refactor: type jwtVerify
-        const jwt = await jose.jwtVerify(trimmedToken, secret);
-        const userRoles = jwt.payload["roles"] as string[]; // Type assertion
+        const jwt = await jose.jwtVerify<accessToken>(trimmedToken, secret);
+        const userRoles = jwt.payload.roles;
 
         if (Array.isArray(userRoles) && userRoles.includes(role)) {
           next();
@@ -58,3 +59,4 @@ export const authorize =
       res.status(403).send();
     }
   };
+
